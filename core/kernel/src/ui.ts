afba9e1a88958794dc3ca6fd643b6f96e59b9eb2
mathiscode <code@mathis.network>
2024-11-28 03:44:45 -0600

    revert auto-login and use env

--- core/kernel/src/ui.ts
+++ core/kernel/src/ui.ts
@@ -5,8 +5,11 @@ declare global {
   var kernel: Kernel | undefined // eslint-disable-line no-var
 }
 
+const username = import.meta.env.VITE_USERNAME || 'root'
+const password = import.meta.env.VITE_PASSWORD || 'root'
+
 globalThis.kernel = new Kernel({
-  credentials: { username: 'root', password: 'root' },
+  credentials: (username && password) ? { username, password } : undefined,
   dom: { topbar: import.meta.env.NODE_ENV !== 'test' },
   log: { name: `ecmaos:${import.meta.env.NODE_ENV || 'kernel'}` },
   socket: import.meta.env.NODE_ENV !== 'test' ? new WebSocket('ws://localhost:30445/socket') : undefined
