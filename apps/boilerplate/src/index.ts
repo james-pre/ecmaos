8c2f1dad9dd6d9e1639fd5800f428ffa55dc96b9
mathiscode <code@mathis.network>
2024-12-09 13:01:18 -0600

    expand boilerplate app demo

--- apps/boilerplate/src/index.ts
+++ apps/boilerplate/src/index.ts
@@ -6,8 +6,10 @@ const main = async (params: ProcessEntryParams) => {
   console.log('Boilerplate App:', params)
   const { args, command, cwd, gid, kernel, pid, shell, terminal, stdin, stdout, stderr, uid } = params
   terminal.writeln(`Hello, ${kernel.name} ${kernel.id}!`)
-  terminal.writeln(`CWD: ${shell.cwd}`)
+  terminal.writeln(`CWD: ${cwd}`)
   terminal.writeln(`ARGS: ${args.join(' ')}`)
+
+  kernel.toast.success('Boilerplate app loaded!')
 }
 
 export default main
