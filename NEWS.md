9d0b6c9e86979c6d86f4e5c73772d7feb0e1ad96
mathiscode <code@mathis.network>
2024-12-13 18:53:33 -0600

    add news app; update deps

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# News
+
+> This file is used by the `@ecmaos-apps/news` app to update the user on the latest news.
+> This is more curated than changesets - it's more for end users and critical updates.
+
+---
+
+## 2024-12-13
+
+- Added `@ecmaos-apps/news` to pull ecmaos news
---
+++ apps/news/src/index.ts
@@ -0,0 +1,19 @@
+#!ecmaos:bin:app:news
+
+import type { ProcessEntryParams } from '@ecmaos/types'
+
+const newsUrl = 'https://raw.githubusercontent.com/ecmaos/ecmaos/main/NEWS.md'
+
+const main = async (params: ProcessEntryParams) => {
+  const { args, terminal } = params
+  const numEntries = parseInt(args[0] || '5')
+  const news = await fetch(newsUrl)
+  const newsData = await news.text()
+  const entries = newsData.split('---').slice(1).slice(0, numEntries)
+
+  for (const entry of entries) {
+    terminal.writeln(entry)
+  }
+}
+
+export default main
